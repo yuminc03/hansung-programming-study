de128b68fa07a56214143095270c056add86c775
Yumin Chu <yuminc03@gmail.com>
2025-11-23 20:22:29 +0900

    feat: 게임 규칙 수정 (게임 시도 코인 입력받기)

--- C-Programming/Task_JackpotGame/Task_JackpotGame/main.c
+++ C-Programming/Task_JackpotGame/Task_JackpotGame/main.c
@@ -7,17 +7,19 @@ int main(void)
 {
 	const int COIN = 1; // 게임 한 판에 사용할 코인의 수
 	const int JACKPOT_REWARD = 80; // 잭팟 상품
-	const int TRIPLE_REWARD = 60; // 트리플 상품
+	const int TRIPLE_REWARD = 40; // 트리플 상품
 
 	srand(time(NULL));
 	int attemptCount = 0; // 게임 시도 횟수
 	int jackpotWins = 0; // 7-7-7 잭팟 횟수 (1이 되었을 때 게임 종료)
 	int tripleWins = 0; // 7-7-7 잭팟 횟수를 제외한 트리플 횟수(예시: 3-3-3)
-	int coins = 100; // 기본으로 갖고 시작할 코인의 수
+	int coins; // 기본으로 갖고 시작할 코인의 수
 	
 	printf("=========================================================\n");
-	printf("잭팟 게임을 시작합니다! 7-7-7이 나오면 게임은 종료됩니다.\n");
+	printf("잭팟 게임을 시작합니다! 코인이 부족하면 게임이 종료됩니다.\n");
 	printf("게임 한 판에 사용할 코인은 %d개 입니다.\n", COIN);
+	printf("게임을 할 때 사용할 코인 개수를 입력하세요: ");
+	scanf("%d", &coins);
 	printf("=========================================================\n");
 	printf("\n");
 
@@ -35,20 +37,29 @@ int main(void)
 		if (num1 == 7 && num2 == 7 && num3 == 7)
 		{
 			// 잭팟 (7-7-7)
-			printf("*************************************\n");
-			printf("<<-------%d-%d-%d 잭팟 성공!------->>\n", num1, num2, num3);
-			printf("<<-------게임 시도 횟수: %d-------->>\n", attemptCount);
+			printf("\n\n");
+			printf("***********************************************\n");
+			printf("<<-------    %d-%d-%d 잭팟 성공!    ------->>\n", num1, num2, num3);
+			printf("<<-------   게임 시도 횟수: %d   -------->>\n", attemptCount);
+			printf("<<-------     + %d 코인!    ------->>\n", JACKPOT_REWARD);
+			printf("***********************************************\n");
+			printf("\n");
+
 			coins += JACKPOT_REWARD;
 			jackpotWins += 1;
-			printf("<<------- + %d 코인!-------->>\n", JACKPOT_REWARD);
 		}
 		else if (num1 == num2 && num2 == num3) {
 			// 잭팟이 아닌 트리플 (3-3-3, 5-5-5 등..)
-			printf("**********************************\n");
-			printf("<<-------%d-%d-%d 트리플!------->>\n", num1, num2, num3);
+			printf("\n\n");
+			printf("********************************************\n");
+			printf("<<-------    %d-%d-%d 트리플!    ------->>\n", num1, num2, num3);
+			printf("<<-------      + %d 코인!     -------->>\n", TRIPLE_REWARD);
+			printf("<<------ 게임 시도 횟수: %d ------>>\n", attemptCount);
+			printf("********************************************\n");
+			printf("\n");
+
 			coins += TRIPLE_REWARD;
 			tripleWins += 1;
-			printf("<<------- + %d 코인!-------->>\n", TRIPLE_REWARD);
 		}
 		else {
 			printf("다음 기회에..\n");
