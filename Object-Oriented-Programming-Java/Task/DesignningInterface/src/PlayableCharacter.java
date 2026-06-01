8104ae6872a6fe8556d627ae49ac78d1a9fbe6b7
Yumin Chu <yuminc03@gmail.com>
2026-06-01 13:07:37 +0900

    refactor: 다형성 상호작용 및 체력 관리를 위한 인터페이스 구조 확장
    - Attackable 인터페이스의 attack() 메서드가 PlayableCharacter 대상을 매개변수로 받도록 변경
    - PlayableCharacter 인터페이스에 takeDamage(), getName(), getHp(), setHp() 추상 메서드 추가

--- Object-Oriented-Programming-Java/Task/DesignningInterface/src/PlayableCharacter.java
+++ Object-Oriented-Programming-Java/Task/DesignningInterface/src/PlayableCharacter.java
@@ -6,11 +6,11 @@ interface Movable {
 
 // 전투 및 공격 능력을 부여하는 독립적인 인터페이스
 interface Attackable {
-    // 캐릭터가 적을 공격하는 행동을 정의하는 추상 메서드
-    void attack();
+    // 캐릭터가 다른 캐릭터를 공격하는 행동을 정의하는 추상 메서드 (다형성 상호작용)
+    void attack(PlayableCharacter target);
 }
 
-// 플레이 가능한 캐릭터로서의 고유 기본 행동을 정의하는 독립적인 인터페이스
+// 플레이 가능한 캐릭터로서의 고유 기본 행동 및 상태 표준을 정의하는 독립적인 인터페이스
 public interface PlayableCharacter {
 
     // 캐릭터의 고유 스킬을 사용하는 행동을 정의하는 추상 메서드
@@ -18,4 +18,16 @@ public interface PlayableCharacter {
 
     // 캐릭터의 현재 상태 및 정보를 보여주는 행동을 정의하는 추상 메서드
     void showStatus();
+
+    // 피해를 입었을 때 처리하는 행동을 정의하는 추상 메서드
+    void takeDamage(int damage);
+
+    // 캐릭터의 이름을 반환하는 추상 메서드
+    String getName();
+
+    // 캐릭터의 현재 체력을 반환하는 추상 메서드
+    int getHp();
+
+    // 캐릭터의 체력을 설정하는 추상 메서드
+    void setHp(int hp);
 }
